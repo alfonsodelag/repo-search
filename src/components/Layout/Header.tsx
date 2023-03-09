b2edc9bfab5fce3b47d04f52b5d346f3a24c0f56
LAPTOP-LJB9KAGQ\G513 <alfonsodelag1@gmail.com>
2023-03-09 17:49:25 +0100

    minor fix

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -6,9 +6,7 @@ const Header: FC = () => {
   return (
     <div className="flex justify-center w-full border-b-2 border-gray-200 bg-black">
       <header className="flex items-center w-full h-16 max-w-768 px-4">
-        <Link href="/">
-          <h3 className="m-0 text-white">GitHub Repo Searcher</h3>
-        </Link>
+        <h3 className="m-0 text-white">GitHub Repo Searcher</h3>
         <a
           className="inline-flex ml-auto text-white"
           href="https://github.com/alfonsodelag/repo-search"
